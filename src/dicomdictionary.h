b31d70693442cd6b073dc222f9dfc8ba8673e48b
rageworx <rageworx@gmail.com>
2016-09-12 15:56:58 +0900

    Updating dicom dictionary (on while) - do not download current build !

--- src/dicomdictionary.h
+++ src/dicomdictionary.h
@@ -9,12 +9,31 @@ namespace DicomImageViewer
     #define VERSION_MAJORT( _wd_ )      (BYTE)(( _wd_ & 0xFF00 ) >> 8 )
     #define VERSION_MINOR( _wd_ )       (BYTE)( _wd & 0x00FF )
 
+    /*
+    "ELEM_STATE_RETIRED" is used to indicate that the corresponding Data Element,
+    SOP Class, or Transfer Syntax has been retired.
+    Retired items are shown italicized.
+    When the name of a retired Data Element has been reused,
+    the retired element has commented, or "ELEM_STATE_TRIAL " in
+    the cases in which the Data Element was used in a Draft For Trial Implementation
+    but not standardized.
+    */
+    enum EDicomDictionaryState
+    {
+        ELEM_STATE_UNKNWON  = 0x00000000,
+        ELEM_STATE_NORMAL   = 0x00000001,
+        ELEM_STATE_TRIAL    = 0x00000002,
+        ELEM_STATE_DICOS    = 0x00000004,
+        ELEM_STATE_DICONDE  = 0x00000008,
+        ELEM_STATE_RETIRED  = 0x00000010,
+    };
+
     typedef struct _TDicomDictionary
     {
-        DWORD   id;
-        char    vr[3];
-        const
-        char*   mean;
+        DWORD       id;
+        const char* vr;
+        const char* mean;
+        int         state;
     }TDicomDictionary;
 
     class DicomDictionary
@@ -27,6 +46,7 @@ namespace DicomImageViewer
             static const char* GetMean(const DWORD id);
             static WORD GetVR(const DWORD id, bool* sameavailed = NULL );
             static void GetVR(const DWORD id, void *pVR, bool* sameavailed = NULL );
+            static int GetElemState( const DWORD id );
             static int GetVRarray( const DWORD id, WORD**pVRarray );
     };
 };
