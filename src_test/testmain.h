51e87d805715772fb2722e0ef1b770d61c7cc76e
Raphael Kim <rageworx@gmail.com>
2021-04-28 14:35:55 +0900

    Supporting MacOS 11

--- src_test/testmain.h
+++ src_test/testmain.h
@@ -1,14 +1,22 @@
 #ifndef __TESTMAIN_H__
 #define __TESTMAIN_H__
 
+#ifdef _WIN32
 #include <windows.h>
 #include <wchar.h>
+#endif /// of _WIN32
+
+#ifndef _MSC_VER
+#include <unistd.h>
+#endif
 
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 
+#ifndef __APPLE__
 #include <io.h>
+#endif /// of __APPLE__
 #include "libdcm.h"
 
 #endif // of __TESTMAIN_H__
